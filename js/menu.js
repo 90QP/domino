d6f2e1e11c7f0dcef2d358b5b552105ebb0b6b64
90QP <157453834+90QP@users.noreply.github.com>
2024-02-23 17:01:57 +0900

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,35 @@ mBtn.addEventListener('click',()=>{
   mBtn.classList.toggle('on');
   mainMenu.classList.toggle('on');
 })
+
+
+// 메뉴 hover시
+const elMenus = document.querySelectorAll('.de_ul > li')
+const elSelect = document.querySelector('.select_bar')
+
+function init(){
+  elMenus[0].classList.add('selected')
+  elSelect.style = `
+      width:${elMenus[0].offsetWidth}px;
+      left:${elMenus[0].offsetLeft}px;
+    `
+}
+init();
+
+window.addEventListener('resize',init);
+
+elMenus.forEach(function(menu,idx){
+  menu.onmouseenter = function(){
+    elMenus[0].classList.remove('selected')
+    this.classList.add('selected');
+
+    elSelect.style = `
+      width:${this.offsetWidth}px;
+      left:${this.offsetLeft}px;
+    `
+  }
+  menu.onmouseleave = function(){
+    this.classList.remove('selected');
+    init();
+  }
+})
