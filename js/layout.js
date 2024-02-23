0247597f593e75826eed228db2fb69564fca31e0
90QP <157453834+90QP@users.noreply.github.com>
2024-02-23 14:33:41 +0900

    Add files via upload

---
+++ js/layout.js
@@ -0,0 +1,24 @@
+const $body = document.querySelector('body');
+const mBtn = document.querySelector('.mobile_btn');
+const mainMenu = document.querySelector('.main_menu');
+
+// 스크롤하면 헤더 메뉴 흰색으로 변경
+window.addEventListener('scroll', ()=>{
+  let scroll = window.scrollY;
+  if( scroll > 100 ){
+    $body.classList.add('scrolling')
+  } else {
+    $body.classList.remove('scrolling')
+  }
+})
+
+// 모바일 - 햄버거 버튼 클릭 시 X로 변경후 메뉴띄움
+mBtn.addEventListener('click',()=>{
+  mBtn.classList.toggle('on');
+  mainMenu.classList.toggle('on');
+})
+
+mainMenu.addEventListener('click',(e)=>{
+  //메뉴에서 링크 없는 버튼 눌렀을때 맨 상단으로 가는 것 방지
+  e.preventDefault();
+})
