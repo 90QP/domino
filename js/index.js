7b124a16666c38939224a8bdbb633107018d7964
Yesom <157453834+90QP@users.noreply.github.com>
2024-02-28 17:31:39 +0900

    메인완성

--- js/index.js
+++ js/index.js
@@ -27,3 +27,37 @@ const swiper = new Swiper('.menu-slider', {
   },
 });
 
+
+
+// 스크롤하면 메인메뉴 올라옴
+
+const orderMenu = document.querySelector('.main_order');
+window.addEventListener('scroll', ()=>{
+  let scroll = window.scrollY;
+  if( scroll > 100 ){
+    orderMenu.classList.add('order_scroll')
+  } else {
+    orderMenu.classList.remove('order_scroll')
+  }
+})
+
+
+
+// 가맹안내 hover 
+const infoCard = document.querySelectorAll('.info_card_wrap')
+const infoArti = document.querySelectorAll('.main_info_wrap > article')
+
+function infoinit(){
+  infoCard[1].classList.add('hover_card')
+}
+infoinit();
+
+infoCard.forEach((card)=>{
+  card.addEventListener('mouseover',()=>{
+    infoCard[1].classList.remove('hover_card')
+    card.classList.add('hover_card')
+  })
+  card.addEventListener('mouseout',()=>{
+    card.classList.remove('hover_card');infoCard[1].classList.add('hover_card')
+  })
+})
