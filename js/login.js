1f8ec18887e57da298202f3dcd15f04f7bf16a11
Yesom <157453834+90QP@users.noreply.github.com>
2024-02-29 10:39:00 +0900

    Update login.js

--- js/login.js
+++ js/login.js
@@ -9,3 +9,12 @@ popBtn.addEventListener('click',()=>{
 popClose.addEventListener('click',()=>{
   pop.classList.remove('popOpen')
 })
+
+const mBtn = document.querySelector('.mobile_btn');
+const mainMenu = document.querySelector('.main_menu');
+
+// 모바일 - 햄버거 버튼 클릭 시 X로 변경후 메뉴띄움
+mBtn.addEventListener('click',()=>{
+  mBtn.classList.toggle('on');
+  mainMenu.classList.toggle('on');
+})
