61152f61a408749a376c8775cd44ca4eb35ca83f
gdr9408 <157453829+gdr9408@users.noreply.github.com>
2024-02-29 16:02:13 +0900

    Add files via upload

--- js/details.js
+++ js/details.js
@@ -8,6 +8,13 @@ let totalPrice = 0;
 
 sizeBtn.forEach((btn)=>{
   btn.addEventListener('click',()=>{
+    sizeBtn.forEach((btns)=>{ 
+      //forEach를 사용해서 각각 버튼의 stop 클래스 삭제
+      btns.classList.remove('stop')
+    })
+    //선택된 버튼에 stop 클래스 추가
+    btn.classList.add('stop')
+
     const price = btn.getAttribute('data-price');
     priceTotal.innerHTML = new Intl.NumberFormat('ko-KR', {
       style: 'currency',  //통화단위
@@ -15,6 +22,7 @@ sizeBtn.forEach((btn)=>{
     }).format(price)
   })
 })
+
 
 
 const minus = document.querySelector('.minus')
